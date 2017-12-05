7b68bb7c52aa75cf87ce4cb59f95da0e1a3d4b83
logonrm <logonrm@fiap.com.br>
2017-12-04 22:42:39 -0200

    App feito na aula 1 de Android Java

---
+++ app/src/main/java/br/com/fiap45401/placarapp/MainActivity.java
@@ -0,0 +1,40 @@
+package br.com.fiap45401.placarapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText etTimeCasa;
+    private EditText etTimeVisitante;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etTimeCasa = (EditText) findViewById(R.id.etTime1);
+        etTimeVisitante = (EditText) findViewById(R.id.etTime2);
+    }
+
+    public void comecarJogo(View v){
+        if (etTimeCasa.getText().toString().isEmpty()) {
+            Toast.makeText(this, "Informe o time da casa", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (etTimeVisitante.getText().toString().isEmpty()) {
+            Toast.makeText(this, "Informe o time visitante", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Intent proxTela = new Intent(this, GameActivity.class);
+        proxTela.putExtra("TIMEVISITANTE", etTimeVisitante.getText().toString());
+        proxTela.putExtra("TIMECASA", etTimeCasa.getText().toString());
+        startActivity(proxTela);
+    }
+}
